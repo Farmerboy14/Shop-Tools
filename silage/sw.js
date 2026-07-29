477fcaff70bb312980e111a6d0e53ac73cb5fcdf
Farmerboy14 <112348671+Farmerboy14@users.noreply.github.com>
2026-07-28 22:53:06 -0600

    Silage Loads: modern-farmer facelift, landscape support, remove quick counter (#9)
    Flat hard-edged dark theme in soil/amber/field-green, two-column landscape layout, bigger stacked dim readout, and removal of the widget.html quick counter.

--- silage/sw.js
+++ silage/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "silage-loads-v7";
-const ASSETS = ["./", "./index.html", "./widget.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+const CACHE = "silage-loads-v9";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
